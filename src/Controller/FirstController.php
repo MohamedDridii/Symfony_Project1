3d2a8b8c3d96f68ceb14ab278bc8d9664682fc2a
MohamedDridii <mouhamed.eldridi@gmail.com>
2025-07-04 12:20:24 +0100

    ajout du controlleur

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'app_first')]
+    public function index(EntityManagerInterface $manager): Response
+    { 
+        $movie=new Movie();
+        $movie->setMovieName('The Matrix');
+        $movie->setDescription('A computer hacker learns about the true nature of his reality and his role in the war against its controllers.');
+        $manager->persist($movie);
+        $manager->flush();
+        return $this->render('first/index.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+}
