364b7b8d8d2e1e4564df19443811693bc2367150
MohamedDridii <mouhamed.eldridi@gmail.com>
2025-07-04 11:57:51 +0100

    created movie entity

---
+++ src/Entity/Movie.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $movie_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMovieName(): ?string
+    {
+        return $this->movie_name;
+    }
+
+    public function setMovieName(string $movie_name): static
+    {
+        $this->movie_name = $movie_name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
