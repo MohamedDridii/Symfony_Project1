76b446db27ae3fe0f96ac222948c3edc5833c790
MohamedDridii <mouhamed.eldridi@gmail.com>
2025-07-04 16:21:44 +0100

    commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Movies;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $movie=new Movies();
+        $movie->setMovieTitle('Inception');
+        $movie->setDescription('A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.');
+        $manager->persist($movie);
+        $movie=new Movies();
+
+        $movie->setMovieTitle('interstellar');
+        $movie->setDescription('A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.');
+        $manager->persist($movie);
+        
+        $movie=new Movies();
+        $movie->setMovieTitle('The Dark Knight');
+        $movie->setDescription('When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.');
+        $manager->persist($movie);
+        
+        $manager->flush();
+    }
+}
